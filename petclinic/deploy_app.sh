a6f46b96ada29519e5a8c246f6e30b735c1a06bc
Rahul <rkrsn@pop-os.localdomain>
2022-07-12 08:01:27 -0400

    Updated petclinic to run locally with a java agent

--- petclinic/deploy_app.sh
+++ petclinic/deploy_app.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+WORK_DIR=$PWD
+# CALL_TRACING_AGENT_PATH=/path/to/agent.jar
+CALL_TRACING_AGENT_PATH=/home/rkrsn/workspace/tackle/tackle-axe/call-tracing-agent/build/libs/ctaagent-1.0-SNAPSHOT.jar
 DOCKER_IMAGE_NAME_REST="springcommunity/spring-petclinic-rest"
 DOCKER_IMAGE_NAME_ANGULAR="spring-petclinic-angular:latest"
 DOCKER_CONTAINER_NAME_REST="petclinic-rest"
@@ -17,24 +20,27 @@ usage() {
 stop() {
     echo "=> Stopping and removing existing $DOCKER_CONTAINER_NAME_ANGULAR container"
     docker rm -f $DOCKER_CONTAINER_NAME_ANGULAR
+    echo "=> Stopping and $DOCKER_CONTAINER_NAME_REST"
+    kill -9 $(jps | grep "spring-petclinic-rest" | cut -d " " -f 1)
 
-    echo "=> Stopping and removing existing $DOCKER_CONTAINER_NAME_REST container"
-    docker rm -f $DOCKER_CONTAINER_NAME_REST
-    sleep 1
+    # echo "=> Stopping and removing existing $DOCKER_CONTAINER_NAME_REST container"
+    # docker rm -f $DOCKER_CONTAINER_NAME_REST
+    # sleep 1
 }
 
 build() {
     echo "=> Building $DOCKER_IMAGE_NAME_ANGULAR"
     cd ./spring-petclinic-angular
     docker build -t spring-petclinic-angular:latest .
+    echo "=> Building $DOCKER_CONTAINER_NAME_REST locally"
+    cd ./spring-petclinic-rest
+    ./mvnw clean package
+    cd $WORK_DIR
 }
 
 start() {
+    "Local deployment for testing"
     stop
-    echo "=> Running $DOCKER_CONTAINER_NAME_REST container"
-    docker run -d -p 9966:9966 --name $DOCKER_CONTAINER_NAME_REST $DOCKER_IMAGE_NAME_REST
-    sleep 3
-
     if [[ $# -eq 1 ]]; then
         echo "=> Building $DOCKER_IMAGE_NAME_ANGULAR"
         cd ./spring-petclinic-angular
@@ -42,6 +48,10 @@ start() {
     fi
     echo "=> Running $DOCKER_CONTAINER_NAME_ANGULAR container"
     docker run -d -p 8080:8080 --name $DOCKER_CONTAINER_NAME_ANGULAR $DOCKER_IMAGE_NAME_ANGULAR
+    cd $WORK_DIR
+    echo "=> Building $DOCKER_CONTAINER_NAME_REST locally"
+    java -javaagent:$CALL_TRACING_AGENT_PATH -cp $CALL_TRACING_AGENT_PATH:$CLASSPATH -jar spring-petclinic-rest/target/spring-petclinic-rest-2.4.2.jar
+
     sleep 1
 }
 
